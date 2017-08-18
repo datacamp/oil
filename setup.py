fe235aa11bf704f33fab6a43eb1da5ed7e6a8440
Michael Chow <machow@princeton.edu>
2017-08-18 16:23:46 -0400

    add install post hook

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 from setuptools.command.develop import develop
+from setuptools.command.install import install
 from subprocess import check_call
 
 class PostDevelopCommand(develop):
@@ -8,9 +9,18 @@ class PostDevelopCommand(develop):
         check_call("./build/dev.sh all".split())
         develop.run(self)
 
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        check_call("./build/dev.sh all".split())
+        install.run(self)
+
 setup(
 	name = 'osh',
 	version = '0.0.0',
 	packages = ['osh', 'core', 'oil', 'opy', 'asdl'],
-        cmdclass = { 'develop': PostDevelopCommand },
+        cmdclass = {
+            'develop': PostDevelopCommand, 
+            'install': PostInstallCommand
+            },
         include_package_data = True)
