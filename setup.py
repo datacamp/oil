21f3f49ff9b8c7db1180697626dc4b56e8797a4a
Jeroen Hermans <jeroen@datacamp.com>
2019-02-18 23:16:00 +0100

    Expose AST parse and dump

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from subprocess import check_call, CalledProcessError
+
+
+def common_install():
+    try:
+        check_call("./build/dev.sh all".split())
+    except CalledProcessError as c:
+        print c
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for installation mode."""
+
+    def run(self):
+        common_install()
+        develop.run(self)
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+
+    def run(self):
+        common_install()
+        install.run(self)
+
+
+setup(
+    name="osh",
+    version="0.0.0",
+    packages=["osh", "core", "opy", "asdl"],
+    install_requires=["typing"],
+    cmdclass={"develop": PostDevelopCommand, "install": PostInstallCommand},
+    include_package_data=True,
+)
